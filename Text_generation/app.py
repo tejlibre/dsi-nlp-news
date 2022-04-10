f6ef285aa5c9e1f8dcfb8f7e02406f2a9733c0da
ChrisMbeva <chrismbev83@gmail.com>
2022-04-10 16:21:08 +0300

    Update app.py
    Added sidebar links.

--- Text_generation/app.py
+++ Text_generation/app.py
@@ -126,7 +126,9 @@ with textgen:
 ## Add links to other pages/ features
 st.sidebar.write("For further analysis select:")
 st.sidebar.write("[Home Page](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Home_Page/app.py)")
-st.sidebar.write("[Sentiment Analysis](https://share.streamlit.io/chrisliti/module3/main/app/app.py)")
-st.sidebar.write("[Discover Topics](https://share.streamlit.io/chrisliti/module3/main/app/app.py)")
+st.sidebar.write("[Sentiment Analysis](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Sentiment_Analysis/app.py)")
+st.sidebar.write("[Emotions WordCloud](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Emotion_Cloud/app.py)")
+st.sidebar.write("[Topic Model Visualization](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Topic_Modeling_Visualization/app.py)")
+st.sidebar.write("[Topic WordCloud](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Topic_Cloud/app.py)")
 
 
