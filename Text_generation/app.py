ed55d49e726bc086442a88f533de3b447a192084
AmyRouillard <rouillardamy@gmail.com>
2022-04-12 08:58:07 +0200

    text generation code cleaned

--- Text_generation/app.py
+++ Text_generation/app.py
@@ -3,79 +3,10 @@
 # -------------
 
 import streamlit as st
-from streamlit import components
-#import plotly.graph_objects as go
-#import plotly.figure_factory as ff
-import pandas as pd
-#from PIL import Image
-
-
-
-
-import numpy as np
-import datetime as dt
-import os
-import re
-import seaborn as sns
-import datetime as dt
-from wordcloud import wordcloud
-import matplotlib.pyplot as plt
-
-#from sklearn.feature_extraction.text import CountVectorizer
-#import pyLDAvis
-#import pyLDAvis.sklearn
-
 from transformers import pipeline
-
-
-#from sklearn.decomposition import LatentDirichletAllocation
-
-#from random import randint
-#from pickle import load
-#import random
-
-# from keras.models import load_model
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.preprocessing.text import Tokenizer
-
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Dense,LSTM,Embedding
-# from tensorflow.keras.utils import to_categorical
-# from pickle import dump,load
 
 ## Switch off warnings
 st.set_option('deprecation.showPyplotGlobalUse', False)
-
-
-# # -------------
-# # Import data
-# # -------------
-
-# #path = "C:/Users/Amy/Desktop/DSI/Module3/"
-# #df_twitter = pd.read_csv(path+'streamlit_data.csv')
-
-
-# path = os.path.dirname(__file__)
-# df_twitter = pd.read_csv(path+'/streamlit_data.csv')
-
-# ## drop missing data
-
-# df_twitter = df_twitter[df_twitter['date'].notnull()]
-
-# ## Convert date feature
-# df_twitter['date'] = pd.to_datetime(df_twitter['date']).dt.date
-# #Add sidebar to the app
-# ## Create sidebar for filtering regions
-# st.sidebar.header("Date Filter")
-
-# dates = st.sidebar.multiselect("Select dates of interest for wordcloud visualization",
-# options = df_twitter['date'].unique(),
-# default = df_twitter['date'].unique())
-
-# ## Filter dataframe by selected dates
-
-# df_twitter = df_twitter[df_twitter["date"].isin(dates)]
 
 ## Add side menu
 ## Add links to other pages/ features
@@ -85,8 +16,6 @@ st.sidebar.write("[Sentiment Analysis](https://share.streamlit.io/tejlibre/dsi-n
 st.sidebar.write("[Emotions WordCloud](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Emotion_Cloud/app.py)")
 st.sidebar.write("[Topic Model Visualization](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Topic_Modeling_Visualization/app.py)")
 st.sidebar.write("[Topic WordCloud](https://share.streamlit.io/tejlibre/dsi-nlp-news/dev/Topic_Cloud/app.py)")
-
-
  
 #Add title and subtitle to the main interface of the app
 st.title('Scoop Finder')
@@ -94,7 +23,6 @@ st.title('Scoop Finder')
 # -------------- 
 # Text generation
 # -------------- 
-
 
 st.markdown("## Text generation")
 textgen = st.container()
